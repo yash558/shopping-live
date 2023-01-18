5851d8e3fe9a5a16e40ea5abe2c91894d77fdea5
Yash Sharma <76558099+yash558@users.noreply.github.com>
2023-01-18 20:09:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
             BusinessLiveScreen.routeName: (context) => const BusinessLiveScreen(),
             BusinessFollower.routeName: (context) => const BusinessFollower(),
           },
-          home: const HomeScreen(),
+          home: const SignUpScreen(),
         ),
       );
 }
